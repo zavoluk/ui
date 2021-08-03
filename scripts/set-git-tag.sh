cd0bd5229a6bef3b6765ba40b902092eb4f08c1e
zavoluk <zavoluk@gmail.com>
2021-08-03 23:55:50 +0300

    chore(project): improved create git tag script

--- scripts/set-git-tag.sh
+++ scripts/set-git-tag.sh
@@ -1,23 +1,17 @@
-# Добавляет актуальный тег по версии из package.json
+# Создает и загружает на сервер текущий тег по версии из package.json без создания билд-коммита
 PACKAGE_VERSION=`node -p "require('./package.json').version"`
 tagName=$PACKAGE_VERSION
-isPush=$1
+tmpBranch='build/'$PACKAGE_VERSION
+currBranch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 
 if [[ `git status --porcelain` ]]; then
+    git checkout -b $tmpBranch
+
     echo 'Set tag' $tagName
 
     # commit pipe
     git add .
     git commit -m 'Build version '$PACKAGE_VERSION
-
-    # check push flag
-    if [ $isPush = '--push' ]; then
-        git push
-
-        echo 'Commit pushed'
-    else
-        echo 'Commit not pushed'
-    fi
 
     # tag pipe
     git tag -d $tagName
@@ -25,6 +19,9 @@ if [[ `git status --porcelain` ]]; then
     git push origin $tagName --force
 
     echo 'Tag '$tagName' added successfully!'
+
+    git checkout $currBranch;
+    git branch -D $tmpBranch
 
 else
   echo 'No git changes'
