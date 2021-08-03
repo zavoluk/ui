867aab61828d7fba6063b830b3ec865dfb1b4a8b
zavoluk <zavoluk@gmail.com>
2021-08-03 23:58:33 +0300

    chore(project): eslint prettier config update

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    arrowParens: "avoid",
+    bracketSpacing: true,
+    doubleQuote: false,
+    endOfLine: "lf",
+    jsxBracketSameLine: false,
+    jsxSingleQuote: true,
+    printWidth: 120,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: "none",
+  };
+  
