78fced433ca11a2011aa8141ecb5b5d17ccc235f
zavoluk <zavoluk@gmail.com>
2021-08-03 23:59:32 +0300

    chore(project): remove dist from git

--- dist/components/Button/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ButtonHTMLAttributes, FC, KeyboardEvent, MouseEvent } from 'react';
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-    onClick?(e: MouseEvent | KeyboardEvent): void;
-}
-declare const Button: FC<Props>;
-export default Button;
