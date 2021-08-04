0960caf9656b328414ff3ee993fc2b87b4f046a2
zavoluk <zavoluk@gmail.com>
2021-08-04 20:49:07 +0300

    chore(project): storybook update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,28 +1,22 @@
-const path = require('path');
-
 module.exports = {
-    stories: ['../src/**/*.story.tsx'],
-    webpackFinal: async config => {
-        config.module.rules.push({
-            test: /\.(ts|tsx)$/,
-            use: [
-                {
-                    loader: require.resolve('ts-loader')
-                },
-                // Optional
-                {
-                    loader: require.resolve('react-docgen-typescript-loader')
-                }
-            ]
-        });
-        config.resolve.extensions.push('.ts', '.tsx');
-        config.resolve.modules.push(path.resolve('src'));
-        return config;
+  "stories": [
+    "../src/**/*.story.mdx",
+    "../src/**/*.story.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    '@storybook/addon-knobs',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
     },
-    addons: [
-        '@storybook/addon-actions/register',
-        '@storybook/addon-knobs/register',
-        '@storybook/addon-notes/register',
-        '@storybook/addon-viewport/register'
-    ]
-};
+  ],
+  core: {
+    builder: "webpack5",
+  }
+}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,25 +2,15 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components';
 import { addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import { withInfo } from '@storybook/addon-info';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 import theme from '../src/Theme'
 
-addDecorator(
-    withInfo({
-        inline: true
-    })
-);
 addDecorator(withKnobs);
 
 addParameters({
     options: {
-        /**
-         * show addon panel as a vertical panel on the right
-         * @type {Boolean}
-         */
-        panelPosition: 'right',
+        panelPosition: 'bottom',
     },
     viewport: {
         viewports: INITIAL_VIEWPORTS
